356ac50c0e7071589f50e7fb6b36c9e9518e59b0
DESKTOP-NJKHAHU\user <mile@naver.com>
2023-07-07 21:45:07 +0900

    (원영도) 1. query 설정

--- src/main/java/com/tawny/spring/test/SpringTestApplication.java
+++ src/main/java/com/tawny/spring/test/SpringTestApplication.java
@@ -1,15 +1,23 @@
 package com.tawny.spring.test;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 @SpringBootApplication
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
+	
 
 }
