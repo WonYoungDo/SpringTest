2a31b34dda6b00645648baea365fcb26ca5a1b77
DESKTOP-NJKHAHU\user <mile@naver.com>
2023-07-18 21:49:27 +0900

    (원영도) 1. jstl 종합문제 공백 추가

--- src/main/java/com/tawny/spring/test/jstl/jstl/WeatherController.java
+++ src/main/java/com/tawny/spring/test/jstl/jstl/WeatherController.java
@@ -18,6 +18,7 @@ public class WeatherController {
 	@Autowired
 	private WeatherService weatherService;
 	
+	
 	@GetMapping("/info")
 	public String weather(Model model) {
 		
@@ -33,4 +34,7 @@ public class WeatherController {
 	public String weatherInput() {
 		return "jstl/weather/weatherInput";
 	}
+	
+	
+	
 }
